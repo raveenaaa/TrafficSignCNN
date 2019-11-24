31d146850bc0932eafaf34568b9454a2cae9b409
Raveena D'costa <rmdcosta@wolftech.ad.ncsu.edu>
2019-11-23 20:20:16 -0500

    Performed Experiment on data7

--- traffic_signs_classification_data_auto.py
+++ traffic_signs_classification_data_auto.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 
 
 # Change these
-choice = 5        # data file (5, 6, 7) are left
+choice = 7        # data file (7, 8) are left
 
 data_filename = "./data/data{}.pickle".format(choice)
 pkl_filename = "./saved_models/data{}_model.pkl".format(choice)
